617baa22e713f630c5015722c9cd0d1a30d67553
Ian Giestas Pauli <iangiestas@usp.br>
2026-04-28 14:16:04 -0300

    qcom-msm8226: Enable USB Gadget Serial (ttyGS0) for console over USB
    - Enable CONFIG_USB_G_SERIAL=y builtin for USB ACM serial console
    - Update boot.kernelParams to use console=ttyGS0,115200
    - Remove unnecessary network/SSH config from stage-1 (no USB debugging needed)
    - Disable DTB (use bootloader DTB instead)
    - Set modular=false since CONFIG_USB_G_SERIAL is now builtin
    - Set device_name to 'titan' to match Motorola device identifier
    - Fix bootimg offsets to standard values (0x00000000 base)

--- devices/qcom-msm8226/default.nix
+++ devices/qcom-msm8226/default.nix
@@ -9,56 +9,39 @@
   mobile.hardware.ram = 1024;
   mobile.hardware.screen = { width = 720; height = 1280; };
 
-  # 🔥 Mainline console (lk2nd routes serial here)
-  mobile.boot.serialConsole = "ttyMSM0,115200n8";
-  mobile.boot.defaultConsole = "ttyMSM0,115200n8";
+  mobile.quirks.fb-refresher.enable = false;
 
   boot.kernelParams = lib.mkForce [
-    "console=ttyMSM0,115200n8"
-    "earlycon=msm_serial_dm,0xf991e000"
+    "console=ttyGS0,115200"
     "loglevel=8"
-    "initrd=0x84000000,16M"
-    "usbcore.autosuspend=-1"
   ];
 
   mobile.boot.stage-1 = {
-    # 🔥 Mainline kernel + modules
     kernel = {
       package = pkgs.callPackage ./kernel { };
-      modular = true;  # Mainline expects modules
+      modular = false;
       useStrictKernelConfig = false;
     };
 
-    # 🔥 USB networking via configfs (no g_android)
-    networking.enable = true;
-    networking.IP = "172.16.42.2";
-    networking.hostIP = "172.16.42.1";
-
-    # 🔥 Debug shell & SSH
-    shell.enable = true;
-    shell.console = "ttyMSM0";
-    ssh.enable = true;
-
-    # 🔥 configfs functions (ACM serial + RNDIS network)
-    usb.features = [ "acm" "rndis" ];
-
     compression = "gzip";
+    networking.enable = false;
+    shell.enable = true;
+    ssh.enable = false;
+    usb.features = [ "acm" "rndis" ];
     extraUtils = lib.mkForce [];
   };
 
-  # 🔥 Android bootimg (lk2nd expects standard header)
   mobile.system.type = "android";
-  mobile.system.android.device_name = "msm8226";
+  mobile.system.android.device_name = "titan";
   mobile.system.android.bootimg.flash = {
-    offset_base = "0x80000000";
-    offset_kernel = "00008000";
-    offset_ramdisk = "01000000";
-    offset_second = "00f00000";  # 🔥 ADDED: Required by the bootimg builder
-    offset_tags = "00000100";
+    offset_base = "0x00000000";
+    offset_kernel = "0x00008000";
+    offset_ramdisk = "0x01000000";
+    offset_second = "0x00f00000";
+    offset_tags = "0x00000100";
     pagesize = "2048";
   };
+  mobile.system.android.bootimg.dt = lib.mkForce null;
 
-  # 🔥 Mainline quirks (postmarketOS verified)
   mobile.quirks.qualcomm.wcnss-wlan.enable = true;
-  mobile.quirks.fb-refresher.enable = true;
 }
